9c18256a6815de2ebaef3cebeebeabb247da846f
Csongor Szasz <szasz.csongor1@gmail.com>
2025-05-15 15:44:46 +0300

    Save t-SNE result to file

---
+++ src/snanomaly/models/results/tsne_result.py
@@ -0,0 +1,10 @@
+from attrs import define, field
+
+from snanomaly.models.results.result import Result
+from snanomaly.models.sncandidate import Bandset
+
+
+@define
+class TSNEResult(Result):
+    sn_name: str = field()
+    bandset: Bandset = field()
